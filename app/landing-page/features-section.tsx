3e64f73f7482632c09fe980adeabb29bcd52004b
yogi03 <yogendrachaurasiya30@gmail.com>
2025-12-25 20:59:18 +0530

    readme update

--- app/landing-page/features-section.tsx
+++ app/landing-page/features-section.tsx
@@ -7,7 +7,7 @@ const features = [
   },
   {
     title: 'Customizable',
-    description: 'Customize Nexx to fit your needs and preferences.',
+    description: 'Customize loopin to fit your needs and preferences.',
     icon: 'icon-2',
   },
   {
@@ -16,11 +16,11 @@ const features = [
   },
   {
     title: 'Secure',
-    description: 'Your data is safe and secure with Nexx.',
+    description: 'Your data is safe and secure with loopin.',
   },
   {
     title: 'Scalable',
-    description: 'Grow your business with Nexx as you scale.',
+    description: 'Grow your business with loopin as you scale.',
   },
   {
     title: 'Fast Support',
